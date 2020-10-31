8e193e44e4134ef88edf923403ee1e8b33293bef
jfranjic1 <jfranjic1@etf.unsa.ba>
2020-10-31 11:06:01 +0100

    very good snake, moves at very good frames, leaves no traces, only problem is that when turning you can see small glitches

--- snaek.py
+++ snaek.py
@@ -10,17 +10,18 @@ NAN = (0,0)
 class Snaek:
     def __init__(self):
         self.lenght = 1
-        self.positions = [(50, 50)]
         self.direction = RIGHT
+        self.positions = [(50, 50, self.direction), (50,51, RIGHT), (50,52, RIGHT)]
         self.color = (17, 24, 47)
-        self.last = self.positions[0]
+        self.last = (50,52, RIGHT)
+        self.lastdirection = self.direction
     def move(self,surface):
-        self.positions.insert(0, (self.positions[0][0] + self.direction[0], self.positions[0][1] + self.direction[1]))
+        self.positions.insert(0, (self.positions[0][0] + self.direction[0], self.positions[0][1] + self.direction[1], self.direction))
         self.last = self.positions.pop()
         for i in range (0,10):
             self.draw(surface, i)
     def changeDirection(self, direction):
-        if (direction == NAN): return
+        if (direction == NAN): return;
         if((self.direction == RIGHT) and (direction == LEFT)):
             return
         if ((self.direction == LEFT) and (direction == RIGHT)):
@@ -30,28 +31,37 @@ class Snaek:
         if (self.direction == DOWN) and (direction == UP):
             return
         self.direction = direction
+
     def draw(self, surface, i):
         snakesize = 10
+        print(self.last)
+        self.lastdirection = self.last[2]
         if(self.direction == RIGHT):
             temp = pygame.Rect(((self.positions[0][0]+i/10)*snakesize,self.positions[0][1]*snakesize),(snakesize/10,snakesize))
             pygame.draw.rect(surface,self.color, temp)
-            temp = pygame.Rect(((self.last[0] + i / 10)* snakesize, self.last[1]*snakesize), (snakesize / 10, snakesize))
-            pygame.draw.rect(surface, (100,100,100), temp)
         if (self.direction == LEFT):
             temp = pygame.Rect(((self.positions[0][0] - i / 10) * snakesize, self.positions[0][1] * snakesize),(snakesize / 10, snakesize))
             pygame.draw.rect(surface, self.color, temp)
-            temp = pygame.Rect(((self.last[0] - i / 10) * snakesize, self.last[1] * snakesize),(snakesize / 10, snakesize))
-            pygame.draw.rect(surface, (100, 100, 100), temp)
         if (self.direction == UP):
             temp = pygame.Rect(((self.positions[0][0]) * snakesize, (self.positions[0][1] - i / 10 )* snakesize),(snakesize , snakesize / 10))
             pygame.draw.rect(surface, self.color, temp)
-            temp = pygame.Rect(((self.last[0]) * snakesize, (self.last[1] - i / 10 )* snakesize),(snakesize, snakesize/10))
-            pygame.draw.rect(surface, (100,100,100),temp)
         if (self.direction == DOWN):
             temp = pygame.Rect(((self.positions[0][0]) * snakesize, (self.positions[0][1] + i / 10 )* snakesize),(snakesize , snakesize / 10))
             pygame.draw.rect(surface, self.color, temp)
-            temp = pygame.Rect((self.last[0] * snakesize, (self.last[1] + i / 10 )* snakesize), (snakesize, snakesize/10))
-            pygame.draw.rect(surface, (100,100,100), temp)
+
+        if (self.lastdirection == RIGHT):
+            temp = pygame.Rect(((self.last[0] + i / 10) * snakesize, self.last[1] * snakesize),(snakesize / 10, snakesize))
+            pygame.draw.rect(surface, (100, 100, 100), temp)
+        if (self.lastdirection == LEFT):
+            temp = pygame.Rect(((self.last[0] - i / 10) * snakesize, self.last[1] * snakesize),(snakesize / 10, snakesize))
+            pygame.draw.rect(surface, (100, 100, 100), temp)
+        if (self.lastdirection == UP):
+            temp = pygame.Rect(((self.last[0]) * snakesize, (self.last[1] - i / 10) * snakesize),
+                               (snakesize, snakesize / 10))
+            pygame.draw.rect(surface, (100, 100, 100), temp)
+        if (self.lastdirection == DOWN):
+            temp = pygame.Rect((self.last[0] * snakesize, (self.last[1] + i / 10) * snakesize),(snakesize, snakesize / 10))
+            pygame.draw.rect(surface, (100, 100, 100), temp)
 
 
 
